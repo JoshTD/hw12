d6047f71951ed9213038929f43884f086db9837a
Josh Khrustalev <khrustalevjosh@gmail.com>
2020-05-05 21:54:32 +0300

    Ver. 1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'hw12'`, () => {
+  it(`should have as title 'proj12'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('hw12');
+    expect(app.title).toEqual('proj12');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('hw12 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('proj12 app is running!');
   });
 });
